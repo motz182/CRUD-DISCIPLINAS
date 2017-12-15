6e41bd09d27b0712441e962ff46dd92d1a255755
motz182 <31115628+motz182@users.noreply.github.com>
2017-12-15 00:55:03 -0200

    feat(insert on db)
    inserindo os dados do formulario na banco de dados do app.

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# CRUD-DISCIPLINAS
+# App notas - Projeto em desenvolvimento #
+
+[![Github-Release](https://img.shields.io/badge/PHP-v7.x-green.svg)](https://github.com/php/php-src/releases)
+[![Github-Release](https://img.shields.io/badge/Laravel-v5.5-green.svg)](https://github.com/laravel/laravel/releases)
+
+### O projeto do app tem como objetivo automatizar o calculo da nota necessaria para passar na disciplina###
+
+O projeto teve inicio no ano de 2017 e foi criado pelo desenvolvedor: 
+- [Moises Reichert](https://www.facebook.com/motz182)
+
+
+O repositorio do projeto se encontra no GitHub:
+
+    https://github.com/motz182/cozinha
+
+# Orientações para os desenvolvedores #
+
+## Convenção de commits ##
+
+### Deve ser utilizado o seguinte padrão para commits: #
+ 
+    tipo(contexto): mensagem
+ 
+### Onde: ###
+
+- **Tipo:** 
+
+    - feat: uma nova feature. 
+    - fix: uma correção. 
+    - docs: alteração apenas em documentação. 
+    - style: alterações que não afetam código (espaço em branco, formatação, etc). 
+    - refactor: melhoria específica de código, que não tem como motivo correção ou adicionar nada. 
+    - perf: melhorias de performance 
+    - test: correção ou adição de testes. 
+    - chore: alterações diversar envolvendo libs, sdks, etc.
+    
+- **Contexto:**
+    
+    - Em qual parte do software foi aplicado a alteração (ex: login, account, invoices, checkout). Caso não se aplique, pode utilizar *. 
+  
+- **Mensagem:**
+    - Mensagem rápida sobre o que foi realizado. 
+ 
+ 
+### Exemplo de commit: ###
+
+    fix(cadUsuario): corrigindo erro quando email é nulo 
--- js/banco.js
+++ js/banco.js
@@ -1,38 +1,26 @@
 window.addEventListener('load', carregado);
+// window.addEventListener('addBanco', salvar);
 
-var db = openDatabase("calculador", "1.0", "app de ", 2 * 1024 * 1024);
-
-db.transaction(function(tx) {
-    tx.executeSql("CREATE TABLE IF NOT EXISTS disciplinas ( id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT ,nome TEXT,n_av INTEGER)" );   
-    tx.executeSql("INSERT INTO disciplinas ( nome,n_av) VALUES ('b', 2)");
-
-});
-
-function carregado(){    
-
+var db;
+function carregado(){
+    db = openDatabase("calculador", "1.0", "app de calculo da nota para passar", 2 * 1024 * 1024);
     db.transaction(function(tx) {
-        //tx.executeSql("DROP TABLE disc" );
-       
-    })
+    tx.executeSql("CREATE TABLE IF NOT EXISTS disciplinas ( id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,nome TEXT,n_av INTEGER)" );   
+    // tx.executeSql("INSERT INTO disciplinas (nome,n_av) VALUES ('b', 2)");
+    //tx.executeSql("DROP TABLE disc" );
+    });
 };
-window.addEventListener('init', function(event) {
-  var page = event.target;
-      if (page.id === 'page2') {
-        page.querySelector('#btn-salvar').onclick = function() {
 
-            function salvar(){
-                var id = document.getElementById('field-id').value;
-                var nome = document.getElementById('field-name').value;
-                var n_av = document.getElementById('field-n_av').value;
-               
-                db.transaction(function(tx) {
-                    if(id){
-                        tx.executeSql('UPDATE disc SET nome=?, n_av=? WHERE id=?', [nome,n_av,id],null);
-                    }else{
-                        tx.executeSql('INSERT INTO disc (nome,n_av) VALUES (?, ?)', [nome,n_av]);
-                    }
-                });
-            }
-        };
-      }
-});
+function get_action(form) {
+    
+    var id = document.getElementById('field-id').value;
+    var nome = document.getElementById('field-name').value;
+    var n_av = document.getElementById('field-n_av').value;
+    // console.log(db); 
+    db.transaction(function(tx) {
+    tx.executeSql('INSERT INTO disciplinas (nome,n_av) VALUES (?, ?)', [nome,n_av]);
+    });
+};
+
+
+           
